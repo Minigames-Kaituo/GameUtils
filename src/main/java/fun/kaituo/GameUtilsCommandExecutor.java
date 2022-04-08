42117ced653b75dc9f75ebcc7577c5a1ef6e5bb8
YFshadaow <renzd2001@gmail.com>
2022-04-08 18:47:38 +0800

    Add placestand command

--- src/main/java/fun/kaituo/GameUtilsCommandExecutor.java
+++ src/main/java/fun/kaituo/GameUtilsCommandExecutor.java
@@ -1,16 +1,18 @@
 package fun.kaituo;
 
 import fun.kaituo.event.PlayerChangeGameEvent;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.Directional;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.util.RayTraceResult;
 
 public class GameUtilsCommandExecutor implements CommandExecutor {
     GameUtils plugin;
@@ -164,7 +166,54 @@ public class GameUtilsCommandExecutor implements CommandExecutor {
                 sender.sendMessage("§c无法重新加入，游戏不存在或者不支持重新加入");
             }
             return true;
-        } else {
+        } else if (cmd.getName().equalsIgnoreCase("placestand")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("§c此指令必须由玩家执行！");
+                return true;
+            }
+            if (!sender.isOp()) {
+                sender.sendMessage("§c你没有权限执行这个指令！");
+                return true;
+            }
+            if (args.length != 1) {
+                sender.sendMessage("§c指令格式错误！");
+            } else {
+                RayTraceResult result = ((Player) sender).rayTraceBlocks(5, FluidCollisionMode.NEVER);
+                if (result != null) {
+                    Block b = result.getHitBlock();
+                    if (b != null) {
+                        if (b.getType().equals(Material.OAK_BUTTON)) {
+                            Location l = b.getLocation().clone();
+                            l.setX(l.getX() + 0.5);
+                            l.setY(l.getY() - 1);
+                            l.setZ(l.getZ() + 0.5);
+                            switch (((Directional)b.getBlockData()).getFacing()) {
+                                case NORTH -> l.setZ(l.getZ() + 0.2);
+                                case SOUTH -> l.setZ(l.getZ() - 0.2);
+                                case EAST -> l.setX(l.getX() - 0.2);
+                                case WEST -> l.setX(l.getX() + 0.2);
+                                case UP, DOWN -> {
+                                    sender.sendMessage("§c按钮方向不可为向上或向下！");
+                                    return true;
+                                }
+                            }
+                            ArmorStand stand = (ArmorStand) Bukkit.getWorld("world").spawnEntity(l, EntityType.ARMOR_STAND);
+                            stand.setInvisible(true);
+                            stand.setCustomName("开始游戏");
+                            stand.setGravity(false);
+                            stand.setInvulnerable(true);
+                        } else {
+                            sender.sendMessage("§c请面对橡木按钮！");
+                        }
+                    } else {
+                        sender.sendMessage("§c请面对橡木按钮！");
+                    }
+                } else {
+                    sender.sendMessage("§c请面对橡木按钮！");
+                }
+            }
+            return true;
+        }else {
             return false;
         }
     }
